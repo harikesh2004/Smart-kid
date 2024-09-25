35ae97f4cafb0a8b166a7fc4f1b459b5c7804ecd
Harikesh <harikeshav99@gmail.com>
2024-09-25 15:22:30 +0530

    Navbar Completed

---
+++ app/_components/Header.tsx
@@ -0,0 +1,70 @@
+"use client"
+import React, { useState } from 'react'
+import {
+    Navbar, 
+    NavbarBrand, 
+    NavbarContent, 
+    NavbarItem, 
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem
+  } from "@nextui-org/navbar";
+import Image from 'next/image'
+import Link from 'next/link';
+import { Button } from '@nextui-org/button';
+
+const Header = () => {
+    const MenuList=[
+        {
+            name:'Home',
+            path:'/' 
+        },
+        {
+            name:'Create Story',
+            path:'/create-story' 
+        },
+        {
+            name:'Explore Story',
+            path:'/explore' 
+        },
+        {
+            name:'Contact Us',
+            path:'/contact-us' 
+        }
+    ]
+const [isMenuOpen,setIsMenuOpen]=useState(false);
+  return (
+    <Navbar maxWidth='full' onMenuOpenChange={setIsMenuOpen}>
+        <NavbarContent>
+            <NavbarMenuToggle aria-label={isMenuOpen?"Close menu":"Open menu"} className='sm:hidden'/>
+            <NavbarBrand>
+                <Image src={'/logo.svg'} alt='logo' width={40} height={40} />
+                <h2 className='font-bold text-2xl text-primary ml-3'>Smart Kid</h2>
+            </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent justify='center'className='hidden sm:flex'>
+            {MenuList.map((item,index)=>(
+                <NavbarItem className='text-xl text-primary font-medium hover:underline mx-2'>
+                    <Link href={item.path}>
+                        {item.name}
+                    </Link>
+                </NavbarItem>
+            ))}
+        </NavbarContent>
+        <NavbarContent justify='end'>
+            <Button color='primary'>Get Started</Button>
+        </NavbarContent>
+        <NavbarMenu>
+            {MenuList.map((item,index)=>(
+                <NavbarMenuItem>
+                    <Link href={item.path}>
+                        {item.name}
+                    </Link>
+                </NavbarMenuItem>
+            ))}
+        </NavbarMenu>
+    </Navbar>
+  )
+}
+
+export default Header
